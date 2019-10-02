bc7c37295c7ed740d3b6ab56aca1daeed0cbb1e1
Naz K. Abadi <nabadi@connected.io>
2019-10-02 13:58:35 -0400

    [NA] Added menu action item
    - Added copies for trending and highest rated
    - Added asset for trending

--- app/src/main/java/me/nazy/MainActivity.java
+++ app/src/main/java/me/nazy/MainActivity.java
@@ -3,12 +3,30 @@ package me.nazy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_sort) {
+            Toast.makeText(MainActivity.this, "Sort action item selected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
